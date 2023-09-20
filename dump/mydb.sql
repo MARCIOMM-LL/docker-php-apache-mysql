bff83d513fefbd851d367874c917c95eb58a78ee
Márcio <m-andremiranda@outlook.com>
2023-09-20 04:44:07 -0300

    Changes

--- dump/mydb.sql
+++ dump/mydb.sql
@@ -1,14 +1,17 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE TABLE 'Person' (
-  'id' int(11) NOT NULL,
-  'name' varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE DATABASE myDb;
 
+CREATE TABLE Person (
+    id int,
+    name varchar(20)
+);
 
-INSERT INTO 'Person' ('id', 'name') VALUES
+INSERT INTO Person (id, name) VALUES
 (1, 'Lilian'),
 (2, 'Márcio'),
 (3, 'André'),
 (4, 'Cristine');
+
+select * from Person;
